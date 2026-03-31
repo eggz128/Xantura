cc08fdb5379d49fd73e4dd33178463d8ad7c2bd5
Steve Powell <steve.powell@edgewords.co.uk>
2026-03-31 11:53:29 +0100

    End of locators

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -9,15 +9,19 @@ test('test', async ({ page }) => { //Always "await" steps that run in browser. T
   //Cy: cy.get('a[src$="auth.php"]').click();
   //Wd: await driver.findElement(By.linkText("Login to restricted area")).click();
   //Old PW: await page.click('a[src$="auth.php"]'); //Dont do this - it works but is considered poor practice today as it doesnt follow the pattern
-  await page.getByRole('link', { name: 'Login To Restricted Area' }).click();
+  //await page.getByRole('link', { name: 'Login To' }).click();
+  await page.getByRole('link', { name: /Login .*/ }).click();
 
   //If an element is to be used multiple times you can define a variable that holds the locator
   //Locators can be "chained" i.e. find ElementA . inside that, find ElementB
   //.describe() can give a "friendly name" to the target element for use in the HTML report. Otherwise the locator chain is used.
   const username = page.getByRole('row', { name: 'User ' }).locator('#username').describe('The username field'); //Unlike WebDriver - no search for the element is performed here in browser. No need to await.
   //The variable can then be used multiple times instead of repeating the entire locator.
+  await page.locator('input').first().fill('test')
+
   await username.click(); //The search for the element starts when an action is called. You *must* await actions, or your tests may flake/fail.
   await username.fill('edgewords');
+  await page.locator('#username').filter({ visible: true }).click();
   //locator() takes CSS or xpath. Prefer CSS.
   await page.locator('#password').click();
   await page.locator('#password').fill('edgewords123');
@@ -55,3 +59,41 @@ test('test', async ({ page }) => { //Always "await" steps that run in browser. T
               - /url: "#"
     `);
 });
+
+test('all products', async ({ page }) => {
+
+  await page.goto('https://www.edgewordstraining.co.uk/demo-site/');
+  const newProducts = page.getByLabel('Recent Products');
+  for (const prod of await newProducts.locator('h2:not(.section-title)').all()) { //gathers a collection of all() matching elements
+    console.log(await prod.textContent()); //then loops over each individual matches logging the text
+  }; //No need to await console, but you do need to await the locator. Or you will only get the "promise" of the text, not the actual text.
+
+});
+
+ test('Locator Handler', async ({ page }) => { //How to handle unpredicatable modal "pop ups"
+    // Setup the handler.
+    const cookieConsent = page.getByRole('heading', { name: 'Hej! You are in control of your cookies.' });
+    await page.addLocatorHandler(
+      cookieConsent, //Locator to watch out for
+      async () => { //If spotted, what to do
+        //console.log("Spotted cookie consent")
+        await page.getByRole('button', { name: 'Accept' }).click();
+      }
+      , //Optional arguments - can be omitted
+      {
+        times: 10, //How many times the locator may appear before the handler should stop handling the locator
+        //By default Playwright will wait for the locator to no longer be visible before continuing with the test.
+        noWaitAfter: false //this can be overridden however
+      }
+    );
+
+    // Now write the test as usual. If at any time the cookie consent form is shown it will be accepted.
+    await page.goto('https://www.ikea.com/');
+    await page.getByRole('link', { name: "40 years of the IKEA meatball" }).click();
+    await expect(page.getByRole('heading', { name: "40 years of having a ball" })).toBeVisible();
+
+    //If you're confident the locator will no longer be found you can de-register the handler
+    //await page.removeLocatorHandler(cookieConsent);
+    //If the cookie consent form appears from here on it may cause issues with the test...
+    await page.waitForTimeout(10000); //Big dumb wait before finishing the test
+  })
