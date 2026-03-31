83b58231306230bb788cf7e9a5379d93848a4083
Steve Powell <steve.powell@edgewords.co.uk>
2026-03-31 15:06:12 +0100

    Capturing Data end

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -30,6 +30,10 @@ test('test', async ({ page }) => { //Always "await" steps that run in browser. T
   //Expects have 5s to complete. The test as a whole has 30s. (By default)
   await expect(page.getByRole('heading', { name: 'Add A Record To the Database' })).toBeVisible();
   await expect(page.locator('h1')).toContainText('Add A Record To the Database');
+
+  let linkText="Submit";
+  let row3cell1 = "Pin?";
+
   await expect(page.locator('#AddRecord')).toMatchAriaSnapshot(`
     - heading "Add" [level=2]
     - table:
@@ -43,7 +47,7 @@ test('test', async ({ page }) => { //Always "await" steps that run in browser. T
           - cell:
             - textbox
         - row "Pin?":
-          - cell "Pin?"
+          - cell "${row3cell1}"
           - cell:
             - textbox
         - row "Password?":
@@ -53,7 +57,7 @@ test('test', async ({ page }) => { //Always "await" steps that run in browser. T
         - row "Submit Clear":
           - cell
           - cell "Submit Clear":
-            - link "Submit":
+            - link "${linkText}":
               - /url: "#"
             - link "Clear":
               - /url: "#"
@@ -70,30 +74,94 @@ test('all products', async ({ page }) => {
 
 });
 
- test('Locator Handler', async ({ page }) => { //How to handle unpredicatable modal "pop ups"
-    // Setup the handler.
-    const cookieConsent = page.getByRole('heading', { name: 'Hej! You are in control of your cookies.' });
-    await page.addLocatorHandler(
-      cookieConsent, //Locator to watch out for
-      async () => { //If spotted, what to do
-        //console.log("Spotted cookie consent")
-        await page.getByRole('button', { name: 'Accept' }).click();
-      }
-      , //Optional arguments - can be omitted
+test('Locator Handler', async ({ page }) => { //How to handle unpredicatable modal "pop ups"
+  // Setup the handler.
+  const cookieConsent = page.getByRole('heading', { name: 'Hej! You are in control of your cookies.' });
+  await page.addLocatorHandler(
+    cookieConsent, //Locator to watch out for
+    async () => { //If spotted, what to do
+      //console.log("Spotted cookie consent")
+      await page.getByRole('button', { name: 'Accept' }).click();
+    }
+    , //Optional arguments - can be omitted
+    {
+      times: 10, //How many times the locator may appear before the handler should stop handling the locator
+      //By default Playwright will wait for the locator to no longer be visible before continuing with the test.
+      noWaitAfter: false //this can be overridden however
+    }
+  );
+
+  // Now write the test as usual. If at any time the cookie consent form is shown it will be accepted.
+  await page.goto('https://www.ikea.com/');
+  await page.getByRole('link', { name: "40 years of the IKEA meatball" }).click();
+  await expect(page.getByRole('heading', { name: "40 years of having a ball" })).toBeVisible();
+
+  //If you're confident the locator will no longer be found you can de-register the handler
+  //await page.removeLocatorHandler(cookieConsent);
+  //If the cookie consent form appears from here on it may cause issues with the test...
+  await page.waitForTimeout(10000); //Big dumb wait before finishing the test
+})
+
+test('Actions', async ({ page }) => {
+  await page.goto('https://www.edgewordstraining.co.uk/webdriver2/');
+
+  const basicLink = page.getByRole('link', { name: 'Access Basic Examples Area' });
+  await basicLink.click();
+  await page.waitForTimeout(2000);
+  await page.goBack();
+  await basicLink.click(); //Will this work?
+
+  await page.getByRole('link', { name: 'Forms' }).click();
+  await expect(page).toHaveTitle(/For.*/i)
+  await expect(page).toHaveURL(/.*forms\.html$/)
+  await page.locator('#textInput').click({position:{x: 1,y:1}});
+  await page.locator('#textInput').fill('Hello World');
+  
+  await expect(page.locator('#textInput')).toHaveScreenshot('textbox.png', { //On first run a "golden sample" is captured and written to disc. Following runs cheack against that sample.
+      threshold: 0.9, //Allowable colour variance 0-1
+      //maxDiffPixelRatio: 0.3, //Allowable different pixels 0-1
+      //maxDiffPixels: 100 //Exact number of pixel allowed to differ
+    }) //Will still fail regardless if the image sizes don't match
+
+  await expect(page.locator('#textInput')).toHaveValue('Hello World')
+  //await expect(page.locator('#textInput')).toBeHidden({timeout: 10000});
+  const slowExpect = expect.configure({timeout: 7000});
+
+  await slowExpect.soft(page.locator('#textInput')).toBeHidden();
+  
+  await page.locator('#textInput').fill('Hello Steve');
+  //await page.locator('#textInput').clear(); //Not like a real user
+  await page.locator('#textInput').press('Control+a'); //More like a real user
+  await page.locator('#textInput').press('Backspace');
+  await page.locator('#textInput').pressSequentially('Hello World', {delay: 500}); //Slow real typing
+  //await page.locator('#textInput').pressSequentially('Hello World'); //Press seq doesnt clear, just adds text
+  await page.locator('#textArea').click();
+  await page.locator('#textArea').fill('Steve\nwas\nhere\n');
+  await page.locator('#checkbox').click(); //Toggle
+  await page.locator('#checkbox').check(); //Force on
+  await page.locator('#checkbox').uncheck(); //Force off
+  await page.locator('#select').selectOption('Selection Two');
+  await page.getByRole('cell', { name: 'One Two Three' }).click();
+  await page.locator('#two').check(); //Radio buttons work with check() uncheck() too.
+  await page.locator('#password').click();
+  await page.locator('#password').fill('password');
+  await page.getByRole('link', { name: 'Submit' }).click();
+  await expect(page.locator('#textInputValue')).toContainText('Hello WorldXXX');
+
+});
+
+   test('Drag drop', async ({ page }) => {
+    await page.goto('/webdriver2/docs/cssXPath.html');
+    //Dragging and dropping one element to another is simple:
+    //await page.locator('div#one').dragTo(page.locator('div#two'))
+
+    //Dragging and dropping one element is a little trickier:
+    await page.dragAndDrop('#slider a', //Source
+      '#slider a', //Target (same)
       {
-        times: 10, //How many times the locator may appear before the handler should stop handling the locator
-        //By default Playwright will wait for the locator to no longer be visible before continuing with the test.
-        noWaitAfter: false //this can be overridden however
-      }
-    );
-
-    // Now write the test as usual. If at any time the cookie consent form is shown it will be accepted.
-    await page.goto('https://www.ikea.com/');
-    await page.getByRole('link', { name: "40 years of the IKEA meatball" }).click();
-    await expect(page.getByRole('heading', { name: "40 years of having a ball" })).toBeVisible();
-
-    //If you're confident the locator will no longer be found you can de-register the handler
-    //await page.removeLocatorHandler(cookieConsent);
-    //If the cookie consent form appears from here on it may cause issues with the test...
-    await page.waitForTimeout(10000); //Big dumb wait before finishing the test
-  })
+        targetPosition: { x: 100, y: 0 }, //How far to drag
+        force: true, //Dont do actionability checks (don't worry that we are dragging the source "outside" of itself)
+        steps: 20
+      } //Smooth the movement over 20 steps
+    )
+  });
---
+++ tests/test-2.spec.ts
@@ -0,0 +1,73 @@
+import { test, expect } from '@playwright/test';
+
+test('Capturing Text', async ({ page }) => {
+
+  await page.goto("https://www.edgewordstraining.co.uk/webdriver2/docs/forms.html");
+  await page.locator('#textInput').fill("Hello World");
+
+  let textPromise = page.locator('#right-column').textContent();
+  console.log("Without 'await'ing textContent() you get a 'promise' of the future text, not the actual text");
+  console.log(textPromise);
+
+  let rightColText = await page.locator('#right-column').textContent(); //Includes whitespace in HTML file
+
+  console.log("The right column text is with textContent is: " + rightColText);
+
+  rightColText = await page.locator('#right-column').innerText(); //Captures text after browser layout has happened (eliminating most whitespace)
+
+  console.log("The right column text is with innertext is: " + rightColText);
+
+  let textBoxText: string = await page.locator('#textInput').textContent() ?? ""; //TS: if textContent() returns null, retuen empty string "" instead
+  console.log("The text box contains" + textBoxText); //blank as <input> has no inner text
+
+  //Using generic $eval to get the browser to return the INPUT text
+  //This will *not* retry or wait
+  textBoxText = await page.$eval('#textInput', (el: HTMLInputElement) => el.value); //el is an in browser HTML element - not a Playwright object at all.
+  
+  console.log("The text box actually contains: " + textBoxText);
+
+  await page.$eval('#textInput', elm => {
+    console.log(typeof (elm)); //Writes to browser's console! Not NodeJS console.
+  });
+
+  await page.locator('#textInput').evaluate(elm => { //using locator().evaluate() the locator will be retried if no element found
+    console.log(elm); //Also outputs to browser console.
+  });
+
+  expect(textBoxText).toBe("Hello World");
+  await expect(page).toHaveTitle("Forms", { timeout: 5000 }) //Override global config with a custom expect timeout
+});
+
+test('Generic methods', { tag: ['@RunMe', '@Smoke'] }, async ({ page }) => {
+
+  await page.goto("https://www.edgewordstraining.co.uk/webdriver2/docs/forms.html")
+
+  const menuLinks = await page.$$eval('#menu a', (links) => links.map((link) => link.textContent))
+  console.log(`There are ${menuLinks.length} links`)
+
+  console.log("The link texts are:")
+
+  for (const iterator of menuLinks) {
+    console.log(iterator!.trim())
+  }
+  //$eval and $$eval DONT retry. If the locator doesnt find a matching element(s) then you get null.
+
+  //Preferred - using retry-able Playwright locators
+  //expect(await page.locator('#menu a').all()).toHaveLength(5) //Collection should have 5 members - do not cotinue until that is true
+  const preferredLinks = await page.locator('#menu a').all();
+  for (const elm of preferredLinks) {
+    // const elmtext = await elm.textContent();
+    // const elmtexttrimmed = elmtext?.trim(); //Combine these two steps below
+    console.log(`${await elm.textContent().then(text => { return text?.trim() })}`)
+  }
+});
+
+test('Waits', async ({ page }) => {
+  await page.goto('https://www.edgewordstraining.co.uk/webdriver2/');
+  await page.getByRole('link', { name: 'Access Basic Examples Area' }).click();
+  await page.getByRole('link', { name: 'Dynamic Content' }).click();
+  await page.locator('#delay').click();
+  await page.locator('#delay').fill('10');
+  await page.getByRole('link', { name: 'Load Content' }).click();
+  
+});
